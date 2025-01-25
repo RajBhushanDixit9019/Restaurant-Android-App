e44649c0bd1b3fb6c45053d43cdd6b7d880115d7
Raj Bhushan Dixit <rajbhushandixit99@gmail.com>
2025-01-25 21:44:05 +0530

    Added the fragments for customers and admin instead of activities.

--- app/src/main/java/com/example/restaurantapp/admin/AdminDashboardActivity.java
+++ app/src/main/java/com/example/restaurantapp/admin/AdminDashboardActivity.java
@@ -6,11 +6,15 @@ import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.restaurantapp.R;
 
 public class AdminDashboardActivity extends AppCompatActivity {
 
+    String Root_Frag = "root_fragment";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,26 +22,29 @@ public class AdminDashboardActivity extends AppCompatActivity {
 
         // Initialize buttons
         Button btnManageMenu = findViewById(R.id.btnManageMenu);
-        Button btnManageUsers = findViewById(R.id.btnManageUsers);
+        Button btnManageStaff = findViewById(R.id.btnManageStaff);
         Button btnViewReports = findViewById(R.id.btnViewReports);
 
+        loadFrag(new ManageMenu(), 0);
         // Set click listener for managing menu
         btnManageMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Navigate to ManageMenuActivity
-                Intent intent = new Intent(AdminDashboardActivity.this, ManageMenuActivity.class);
-                startActivity(intent);
+                //Intent intent = new Intent(AdminDashboardActivity.this, ManageMenuActivity.class);
+                //startActivity(intent);
+                loadFrag(new ManageMenu(), 0);
             }
         });
 
         // Set click listener for managing users
-        btnManageUsers.setOnClickListener(new View.OnClickListener() {
+        btnManageStaff.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Navigate to ManageUsersActivity
-                Intent intent = new Intent(AdminDashboardActivity.this, ManageUsersActivity.class);
-                startActivity(intent);
+                //Intent intent = new Intent(AdminDashboardActivity.this, ManageUsersActivity.class);
+                //startActivity(intent);
+                loadFrag(new ManageStaff(), 0);
             }
         });
 
@@ -46,9 +53,29 @@ public class AdminDashboardActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // Navigate to ViewReportsActivity
-                Intent intent = new Intent(AdminDashboardActivity.this, ViewReportsActivity.class);
-                startActivity(intent);
+                //Intent intent = new Intent(AdminDashboardActivity.this, ViewReportsActivity.class);
+                //startActivity(intent);
+                loadFrag(new ViewReports(), 0);
             }
         });
+    }
+    // flag 0 for add, 1 for replace
+    public void loadFrag(Fragment fragment_name, int flag)
+    {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        if (flag == 0) {
+            ft.add(R.id.FL, fragment_name);
+
+            fm.popBackStack(Root_Frag, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+            ft.addToBackStack(Root_Frag);
+        }
+        else {
+            ft.replace(R.id.FL, fragment_name);
+            ft.addToBackStack(null);
+        }
+
+        ft.commit();
     }
 }
--- app/src/main/java/com/example/restaurantapp/customer/CustomerDashboardActivity.java
+++ app/src/main/java/com/example/restaurantapp/customer/CustomerDashboardActivity.java
@@ -6,6 +6,9 @@ import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.restaurantapp.R;
 
@@ -13,6 +16,8 @@ public class CustomerDashboardActivity extends AppCompatActivity {
 
     private Button btnViewMenu;
     private Button btnOrderHistory;
+
+    String Root_Frag = "root_fragment";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +28,16 @@ public class CustomerDashboardActivity extends AppCompatActivity {
         btnViewMenu = findViewById(R.id.btnViewMenu);
         btnOrderHistory = findViewById(R.id.btnOrderHistory);
 
+        loadFrag(new ViewMenu(), 0);
         // Set click listener for viewing menu
         btnViewMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Navigate to ViewMenuActivity
-                Intent intent = new Intent(CustomerDashboardActivity.this, ViewMenuActivity.class);
-                startActivity(intent);
+               // Intent intent = new Intent(CustomerDashboardActivity.this, ViewMenuActivity.class);
+               // startActivity(intent);
+                loadFrag(new ViewMenu(), 0);
+
             }
         });
 
@@ -38,9 +46,26 @@ public class CustomerDashboardActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // Navigate to OrderHistoryActivity
-                Intent intent = new Intent(CustomerDashboardActivity.this, OrderHistoryActivity.class);
-                startActivity(intent);
+                //Intent intent = new Intent(CustomerDashboardActivity.this, OrderHistoryActivity.class);
+                //startActivity(intent);
+                loadFrag(new OrderHistory(), 0);
             }
         });
+    }
+    public void loadFrag(Fragment fragment_name, int flag){
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        if(flag == 0){
+            ft.add(R.id.FL, fragment_name);
+
+            fm.popBackStack(Root_Frag, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+            ft.addToBackStack(Root_Frag);
+
+        }else{
+            ft.replace(R.id.FL, fragment_name);
+           ft.addToBackStack(null);
+        }
+        ft.commit();
     }
 }
